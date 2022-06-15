0d9db7fef2c3ad88316bee3fea95cf40f5c746df
baria aziz <bariaziz1996@gmail.com>
2022-06-15 21:32:20 +0100

    add help text, style fixes, minor modifications

--- src/App.js
+++ src/App.js
@@ -2,8 +2,24 @@ import "./App.css";
 import DTGBar from "./DTGBar";
 
 function App() {
+  // todo: button to go to the latest issue
+  const openHelpAlert = () => {
+    alert(`
+    This app has 2 main components:
+    1) Issues Bar
+        - click on different issues to see DTGs from the model 'ecop'
+        - scrolling horizontally on this bar shows the rest of  the list
+    2) DTGs Container
+        - click on different buttons to see the DTG as a text string in UTC
+        - the selected DTG is displayed in bold below all the DTG buttons
+    `);
+  };
+
   return (
     <div className="App">
+      <button className="help-button" onClick={openHelpAlert}>
+        Click me for help
+      </button>
       <DTGBar />
     </div>
   );
